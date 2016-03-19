6ff6cb5875c0c1141aa51f16bfdeed88f4a39819
Emeka Mosanya <emeka@mosanya.net>
2016-03-19 01:02:19 +0000

    Challenge Task 3
    Since demo.war has an embedded Jetty, I have removed the tomcat install
    and directly used java to start the server using systemd service.
    This configuration is closer to a simple 'one process in one container'
    mantra...
    
    Demo is still running as root and need to test puppet dependencies,
    for example does the server is restarted when the environment changes.

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@ Vagrant.configure('2') do |config|
   config.vm.hostname = 'devops-challenge'
   config.vm.box = 'puppetlabs/centos-7.0-64-puppet'
 
-  forwarded_ports = {'80' => '18080', '8000' => '8000', '1234' => '1234' }
+  forwarded_ports = {'80' => '18080', '8000' => '8000' }
 
   forwarded_ports.each do |k,v|
     config.vm.network :forwarded_port, guest: "#{k}",  host: "#{v}", auto_correct: true
--- environments/development/manifests/demo_app.pp
+++ environments/development/manifests/demo_app.pp
@@ -1,2 +1,45 @@
 class demo_app {
+
+  package { 'java':
+    ensure => present
+  }
+
+  file { '/etc/default/demo':
+    ensure  => file,
+    source  => 'puppet:///modules/demo_app/demo.environment',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root'
+  }
+  
+  file { '/usr/local/lib/demo':
+    ensure => directory
+  }
+
+  file { '/usr/local/lib/demo/demo.war':
+    ensure  => file,
+    source  => 'puppet:///modules/demo_app/demo.war',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root'
+  }
+
+  file { '/usr/lib/systemd/system/demo.service':
+    ensure => file,
+    source => 'puppet:///modules/demo_app/demo.service',
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root'
+  }
+
+  service { 'demo':
+    ensure => running,
+    enable => true,
+    require => Package['java'],
+    subscribe => File[
+        '/usr/local/lib/demo/demo.war',
+        '/usr/lib/systemd/system/demo.service',
+        '/etc/default/demo']
+  }
+
 }
